b398a0b5fa15261a2f49d8fa4837640d7f8f505d
MariaPas235 <151913868+MariaPas235@users.noreply.github.com>
2024-11-20 20:46:22 +0100

    Clases creadas

---
+++ src/main/java/org/example/Model/Usuario.java
@@ -0,0 +1,97 @@
+package org.example.Model;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Usuario {
+    private int id_usuario;
+    private String nombre;
+    private String email;
+    private String contrasena;
+    private boolean administrador;
+    private ArrayList<Videojuego> videojuegosCarrito;
+
+    public Usuario(int id_usuario, String nombre, String email, String contrasena, boolean administrador, ArrayList<Videojuego> videojuegosCarrito) {
+        this.id_usuario = id_usuario;
+        this.nombre = nombre;
+        this.email = email;
+        this.contrasena = contrasena;
+        this.administrador = administrador;
+        this.videojuegosCarrito = videojuegosCarrito;
+    }
+
+    public Usuario() {}
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public boolean isAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        this.administrador = administrador;
+    }
+
+    public ArrayList<Videojuego> getVideojuegosCarrito() {
+        return videojuegosCarrito;
+    }
+
+    public void setVideojuegosCarrito(ArrayList<Videojuego> videojuegosCarrito) {
+        this.videojuegosCarrito = videojuegosCarrito;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(email, usuario.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id_usuario=" + id_usuario +
+                ", nombre='" + nombre + '\'' +
+                ", email='" + email + '\'' +
+                ", contrasena='" + contrasena + '\'' +
+                ", administrador=" + administrador +
+                ", videojuegosCarrito=" + videojuegosCarrito +
+                '}';
+    }
+}
